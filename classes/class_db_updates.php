f22a15c8ca399cf1d3b9d9e823b898947bd1152a
root <root@sendthatfile.me>
2012-09-19 03:10:54 +0200

    Update Database class

--- classes/class_db_updates.php
+++ classes/class_db_updates.php
@@ -13,16 +13,17 @@ class UPDATE_DATABASE {
     
     function __construct() {
       $this->populateVersions();
-      $this->update();
+      $this->updateDatabase();
     }
     
-    function update() {
-      $global $DB;
+    function updateDatabase() {
+      global $DB;
       
       $DB->query("select databaseVersion from system");
       list($databaseVersion) = $DB->next_record();
       
-      foreach($versions as $version => $ddl) {
+      foreach($this->versions as $version => $ddl) {
+        $version = floatval($version);
         if ($databaseVersion < $version) {
           $DB->query($ddl); /// update metadata
           $DB->query("update system set databaseVersion = $version"); /// set the new database version
@@ -44,21 +45,21 @@ class UPDATE_DATABASE {
       // Add all your MySQL DDL here following the pattern of version 1.001
       //-----------------------------------------------------------------------------------    
       
-      $versions[1.001] = <<<EOT
---- Version 1.001 will be just a comment
+      $this->versions['1.001'] = <<<EOT
+/* --- Version 1.001 will be just a comment --- */
 EOT;
 
-      $versions[1.002] = <<<EOT
---- Version 1.002 will create a table into the metadata
+      $this->versions['1.002'] = <<<EOT
+/* --- Version 1.002 will create a table into the metadata --- */
 
 CREATE TABLE `language` (
   `LanguageID` int(10) NOT NULL AUTO_INCREMENT,
-  `OriginalName` varchar(128) CHARACTER SET utf8 COLLATE utf8_swedish_ci DEFAULT NOT NULL,
-  `EnglishName` varchar(64) CHARACTER SET utf8 COLLATE utf8_swedish_ci DEFAULT NOT NULL,
   `CountryID` integer NOT NULL,
+  `OriginalName` varchar(128) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
+  `EnglishName` varchar(64) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
   PRIMARY KEY (`LanguageID`),
   UNIQUE KEY `Name` (`EnglishName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 
 EOT;
       
