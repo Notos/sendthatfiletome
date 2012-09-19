4f7f6138eaaf65c5ece4313bd598eda20bca7303
Notos <antoniocarlos.demonoid@cys.com.br>
2012-09-19 01:47:09 -0300

    dd
    Signed-off-by: Notos <antoniocarlos.demonoid@cys.com.br>

--- classes/class_db_updates.php
+++ classes/class_db_updates.php
@@ -464,7 +464,7 @@ CREATE TABLE `language` (
   `OriginalName` varchar(128) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
   `Enabled` boolean DEFAULT false,
   PRIMARY KEY (`LanguageID`, `CountryCode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
 /* ---- Inserts ----- */
