a2e15cf146772f9b9447b428b7ec897f8b08bc6f
Notos <antoniocarlos.demonoid@cys.com.br>
2012-09-20 18:19:53 -0300

    tr

--- sections/tools/managers/translator_list.php
+++ sections/tools/managers/translator_list.php
@@ -12,14 +12,19 @@ show_header('Translator Manager');
 				<td colspan="2">Translate it!</td>
 			</tr>
 			<form>
-  			<tr><td colspan="2"><strong><? echo TOOLS::languageName('EN-US');?></strong></td></tr>
+  			<tr><td colspan="2"><strong><? echo TOOLS::languageName('EN-US');?> - Original message or code</strong></td></tr>
         <tr><td>
-          <textarea name="originalText" cols="55" rows="15"></textarea>
+          <textarea name="originalText" cols="70" rows="5" readonly="readonly"></textarea>
         </td></tr>
 
-  			<tr><td colspan="2"><strong><? echo TOOLS::languageName('PT-BR');?></strong></td></tr>
+  			<tr><td colspan="2"><strong><? echo TOOLS::languageName('EN-US');?> - Message translated to English (this is what you will see)</strong></td></tr>
         <tr><td>
-          <textarea name="originalText" cols="55" rows="15"></textarea>
+          <textarea name="originalText" cols="70" rows="5" readonly="readonly"></textarea>
+        </td></tr>
+
+  			<tr><td colspan="2"><strong><? echo TOOLS::languageName('PT-BR');?> - Your translation goes here</strong></td></tr>
+        <tr><td>
+          <textarea name="originalText" cols="70" rows="15"></textarea>
         </td></tr>
 
         <tr><td>
