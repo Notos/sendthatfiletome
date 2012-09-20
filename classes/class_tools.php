e71608045999591194bed60a91a656fa9e2da7ef
Notos <antoniocarlos.demonoid@cys.com.br>
2012-09-20 11:09:00 -0300

    tr
    Signed-off-by: Notos <antoniocarlos.demonoid@cys.com.br>

--- classes/class_tools.php
+++ classes/class_tools.php
@@ -7,14 +7,14 @@ class TOOLS {
     
     $DB->query($query); /// this query result must have only 2 fields (index and value)
     $records = $DB->to_array(0, MYSQLI_NUM);
-    
+
     echo "<select name=\"$name\" id=\"$name\" onchange=\"$onChange\">";
     echo "<option value=\"\">---</option>";
     foreach($records as $record) {
       $index = $record[0];
       $value = $record[1];
       echo "<option value=\"$index\"";
-      if($value == $currentValue) { echo " selected=\"selected\""; }
+      if($index == $currentValue) { echo " selected=\"selected\""; }
       echo ">";
       echo $value;
       echo "</option>\n";
