6542d2394e2dd1e26f6c7f55de6a903e74d76656
Notos <antoniocarlos.demonoid@cys.com.br>
2012-09-20 23:22:39 -0300

    tr
    Signed-off-by: Notos <antoniocarlos.demonoid@cys.com.br>

--- sections/tools/managers/translator_update.php
+++ sections/tools/managers/translator_update.php
@@ -6,10 +6,6 @@ if(!check_perms('site_translator')){
     error(403);
 }
 
-TOOLS::pa($_REQUEST);
-TOOLS::pa($_POST);
-TOOLS::pa($_GET);
-
 $messageHash = $_POST['messageHash'];
 $originalMessage = $_POST['originalMessage'];
 $englishTranslation = $_POST['englishTranslation'];
@@ -17,15 +13,11 @@ $currentTranslation = $_POST['currentTranslation'];
 $languageID = $_POST['languageID'];
 $countryCode = $_POST['countryCode'];
 
-//$DB->query("update message set TranslatedMessage = '$currentTranslation' where m.EnglishMessageHash = '$messageHash' and m.LanguageID = '$languageID' and m.CountryCode = '$countryCode'");
-echo "update message set TranslatedMessage = '$currentTranslation' where m.EnglishMessageHash = '$messageHash' and m.LanguageID = '$languageID' and m.CountryCode = '$countryCode'";
-echo "insert into message (LanguageID, CountryCode, EnglishMessageHash, EnglishMessage, TranslatedMessage) values ('$languageID','$countryCode','$messageHash','$originalMessage',$currentTranslation')";
+$DB->query("update message set TranslatedMessage = '$currentTranslation' where m.EnglishMessageHash = '$messageHash' and m.LanguageID = '$languageID' and m.CountryCode = '$countryCode'");
 
 if ($DB->affected_rows() == 0) {
-//  $DB->query("insert into message (LanguageID, CountryCode, EnglishMessageHash, EnglishMessage, TranslatedMessage) values ('$languageID','$countryCode','$messageHash','$originalMessage',$currentTranslation')");
-echo "insert into message (LanguageID, CountryCode, EnglishMessageHash, EnglishMessage, TranslatedMessage) values ('$languageID','$countryCode','$messageHash','$originalMessage',$currentTranslation')";
+  $DB->query("insert into message (LanguageID, CountryCode, EnglishMessageHash, EnglishMessage, TranslatedMessage) values ('$languageID','$countryCode','$messageHash','$originalMessage',$currentTranslation')");
 }
 
-die;
 header('Location: tools.php?action=translator');
 ?>
