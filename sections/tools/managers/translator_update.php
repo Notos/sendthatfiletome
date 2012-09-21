b7616c061db719a8ea37f6198888746b16858f44
Notos <antoniocarlos.demonoid@cys.com.br>
2012-09-20 23:20:53 -0300

    tar
    Signed-off-by: Notos <antoniocarlos.demonoid@cys.com.br>

--- sections/tools/managers/translator_update.php
+++ sections/tools/managers/translator_update.php
@@ -3,18 +3,23 @@
 //--------------- Insert or Update a translation -------------------------------//
 
 if(!check_perms('site_translator')){
-	error(403);
+    error(403);
 }
 
-$messageHash = $_GET['messageHash'];
-$originalMessage = $_GET['originalMessage'];
-$englishTranslation = $_GET['englishTranslation'];
-$currentTranslation = $_GET['currentTranslation'];
-$languageID = $_GET['languageID'];
-$countryCode = $_GET['countryCode'];
+TOOLS::pa($_REQUEST);
+TOOLS::pa($_POST);
+TOOLS::pa($_GET);
+
+$messageHash = $_POST['messageHash'];
+$originalMessage = $_POST['originalMessage'];
+$englishTranslation = $_POST['englishTranslation'];
+$currentTranslation = $_POST['currentTranslation'];
+$languageID = $_POST['languageID'];
+$countryCode = $_POST['countryCode'];
 
 //$DB->query("update message set TranslatedMessage = '$currentTranslation' where m.EnglishMessageHash = '$messageHash' and m.LanguageID = '$languageID' and m.CountryCode = '$countryCode'");
 echo "update message set TranslatedMessage = '$currentTranslation' where m.EnglishMessageHash = '$messageHash' and m.LanguageID = '$languageID' and m.CountryCode = '$countryCode'";
+echo "insert into message (LanguageID, CountryCode, EnglishMessageHash, EnglishMessage, TranslatedMessage) values ('$languageID','$countryCode','$messageHash','$originalMessage',$currentTranslation')";
 
 if ($DB->affected_rows() == 0) {
 //  $DB->query("insert into message (LanguageID, CountryCode, EnglishMessageHash, EnglishMessage, TranslatedMessage) values ('$languageID','$countryCode','$messageHash','$originalMessage',$currentTranslation')");
