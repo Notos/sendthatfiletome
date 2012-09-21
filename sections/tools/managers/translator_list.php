c95a9e68bdb9aef83c92ef5c4af8c6bfa327380f
Notos <antoniocarlos.demonoid@cys.com.br>
2012-09-21 00:00:50 -0300

    tr
    Signed-off-by: Notos <antoniocarlos.demonoid@cys.com.br>

--- sections/tools/managers/translator_list.php
+++ sections/tools/managers/translator_list.php
@@ -44,7 +44,7 @@ list($messageHash, $originalMessage, $englishTranslation, $currentTranslation) =
         <td colspan="2">Translate it!</td>
       </tr>
 <?if ( !isset($messageHash) or empty($messageHash) ) {
-      echo '<tr><td colspan="2"><br><br><strong>There are no untranslated messages for '.TOOLS::languageName($language).'</strong><br><br></td></tr>';
+      echo '<tr><td colspan="2"><br><br><strong>There are no untranslated messages for '.TOOLS::languageName($language).'</strong><br><br><br></td></tr>';
 } else { /// start of form block ?>
       <form action="tools.php" method="post" class="pad">
         <input type="hidden" name="action" value="translator_update" />
