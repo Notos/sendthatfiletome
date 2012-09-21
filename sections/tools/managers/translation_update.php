effa0842480149acf6bf772eb508272cf7652bc0
Notos <antoniocarlos.demonoid@cys.com.br>
2012-09-20 23:10:42 -0300

    tr
    Signed-off-by: Notos <antoniocarlos.demonoid@cys.com.br>

---
+++ sections/tools/managers/translation_update.php
@@ -0,0 +1,26 @@
+<?
+//******************************************************************************//
+//--------------- Insert or Update a translation -------------------------------//
+
+if(!check_perms('site_translator')){
+	error(403);
+}
+
+$messageHash = $_POST['messageHash'];
+$originalMessage = $_POST['originalMessage'];
+$englishTranslation = $_POST['englishTranslation'];
+$currentTranslation = $_POST['currentTranslation'];
+$languageID = $_POST['languageID'];
+$countryCode = $_POST['countryCode'];
+
+//$DB->query("update message set TranslatedMessage = '$currentTranslation' where m.EnglishMessageHash = '$messageHash' and m.LanguageID = '$languageID' and m.CountryCode = '$countryCode'");
+echo "update message set TranslatedMessage = '$currentTranslation' where m.EnglishMessageHash = '$messageHash' and m.LanguageID = '$languageID' and m.CountryCode = '$countryCode'";
+
+if ($DB->affected_rows() == 0) {
+//  $DB->query("insert into message (LanguageID, CountryCode, EnglishMessageHash, EnglishMessage, TranslatedMessage) values ('$languageID','$countryCode','$messageHash','$originalMessage',$currentTranslation')");
+echo "insert into message (LanguageID, CountryCode, EnglishMessageHash, EnglishMessage, TranslatedMessage) values ('$languageID','$countryCode','$messageHash','$originalMessage',$currentTranslation')";
+}
+
+die;
+header('Location: tools.php?action=translator');
+?>
